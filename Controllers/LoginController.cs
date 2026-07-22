5712114444f6b086192a9b75f768374605cbc9bd
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-07-22 19:13:29 +0530

    Create ViewModel and Controller for Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project_Job_Portal.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginPageLoad()
+        {
+            return View();
+        }
+
+        public ActionResult EmployeeHome()
+        {
+            return View();
+        }
+
+        public ActionResult CompanyHome()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project_Job_Portal.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Enter username")]
+        public string Username { set; get; }
+
+        [Required(ErrorMessage = "Enter password")]
+        public string Password { set; get; }
+        public string Message { set; get; }
+        public string UserType { set; get; }
+    }
+}
