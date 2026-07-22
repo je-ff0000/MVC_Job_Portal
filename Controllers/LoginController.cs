09274c60589bdbad7fe0b0ca1689418672f0c1e8
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-07-22 19:53:56 +0530

    Add logic in LoginController and design code for View

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Project_Job_Portal.Models;
 
 namespace MVC_Project_Job_Portal.Controllers
 {
     public class LoginController : Controller
     {
+        MVC_Project_Job_PortalEntities dbobj = new MVC_Project_Job_PortalEntities();
         // GET: Login
         public ActionResult LoginPageLoad()
         {
@@ -22,6 +24,48 @@ namespace MVC_Project_Job_Portal.Controllers
         public ActionResult CompanyHome()
         {
             return View();
+        }
+
+        public ActionResult LoginClick(UserLogin clsobj)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var val = dbobj.sp_LoginCountId(clsobj.Username, clsobj.Password).FirstOrDefault();
+                    if(val == 1)
+                    {
+                        var regid = dbobj.sp_GetRegId(clsobj.Username, clsobj.Password).FirstOrDefault();
+                        Session["RegId"] = regid;
+
+                        var logtype = dbobj.sp_LoginType(clsobj.Username, clsobj.Password).FirstOrDefault();
+                        if(logtype == "Employee")
+                        {
+                            return RedirectToAction("EmployeeHome");
+                        }
+                        else if (logtype == "Company")
+                        {
+                            return RedirectToAction("CompanyHome");
+                        }
+                        else
+                        {
+                            TempData["Message"] = "Can't find user type";
+                            return RedirectToAction("LoginPageLoad");
+                        }
+                    }
+                    else
+                    {
+                        clsobj.Message = "Invalid Username or Password";
+                        return View("LoginPageLoad", clsobj);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    clsobj.Message = ex.Message;
+                    return View("LoginPageLoad", clsobj);
+                }
+            }
+            return View("LoginPageLoad", clsobj);
         }
     }
 }
