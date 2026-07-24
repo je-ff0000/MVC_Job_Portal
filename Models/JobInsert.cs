215a25a6775583decea1c72f90432e9a56d02731
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-07-24 21:52:59 +0530

    Add jobinsert model and some view code

---
+++ Models/JobInsert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project_Job_Portal.Models
+{
+    public class JobInsert
+    {
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string SkillsRequired { get; set; }
+        public int ExpRequired { get; set; }
+        public string Qualifiation { get; set; }
+        public decimal Salary { get; set; }
+        public string JobType { get; set; } //dropdown
+        public string Location { get; set; }
+        public DateTime LastDate { get; set; }
+        public DateTime PostedDate { get; set; }
+
+    }
+}
