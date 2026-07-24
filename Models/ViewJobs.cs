1f21c1a4fb931ee6c7c7dd3529c6cbfbff0cdc78
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-07-24 21:32:00 +0530

    Add View for viewpostedjobs and viewjob.cs model

---
+++ Models/ViewJobs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project_Job_Portal.Models
+{
+    public class ViewJobs
+    {
+        public int JobId { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string Location { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime LastDate { get; set; }
+        public string JobType { get; set; }
+    }
+}
