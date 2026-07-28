4b7d1c97edba1a64d72ce1e54350ed6a64b54d36
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-07-28 15:48:13 +0530

    Add validation for properties in JobInsert model and remove PostedDate property

--- Models/JobInsert.cs
+++ Models/JobInsert.cs
@@ -8,16 +8,31 @@ namespace MVC_Project_Job_Portal.Models
 {
     public class JobInsert
     {
+        [Required]
         public string JobTitle { get; set; }
-        public string JobDescription { get; set; }
-        public string SkillsRequired { get; set; }
-        public int ExpRequired { get; set; }
-        public string Qualification { get; set; }
-        public decimal Salary { get; set; }
-        public string JobType { get; set; } //dropdown
-        public string Location { get; set; }
-        public DateTime LastDate { get; set; }
-        public DateTime PostedDate { get; set; }
 
+        [Required]
+        public string JobDescription { get; set; }
+
+        [Required]
+        public string SkillsRequired { get; set; }
+
+        [Required]
+        public int ExpRequired { get; set; }
+
+        [Required]
+        public string Qualification { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+
+        [Required(ErrorMessage = "Select Job Type")]
+        public string JobType { get; set; } //dropdown
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public DateTime LastDate { get; set; }
     }
 }
